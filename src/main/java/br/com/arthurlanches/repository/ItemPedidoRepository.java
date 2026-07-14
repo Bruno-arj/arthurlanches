371970b1db795b8e1016376f2a7358ad87106a64
Nuno <brunoaraujosume@gmail.com>
2026-07-13 23:26:38 -0300

    Movendo as pastas para o lugar e criando o repository

---
+++ src/main/java/br/com/arthurlanches/repository/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package br.com.arthurlanches.repository;
+
+import br.com.arthurlanches.model.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
